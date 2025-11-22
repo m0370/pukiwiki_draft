b480193b3bd05af6d28ab40493cb87bd5a706e5a
m0370 <96211174+m0370@users.noreply.github.com>
2025-11-22 12:46:59 +0900

    衝突時の差分表示と強制公開ボタンを追加

--- plugin/draft.inc.php
+++ plugin/draft.inc.php
@@ -43,6 +43,8 @@ function plugin_draft_action()
 			return plugin_draft_delete();
 		case 'publish':
 			return plugin_draft_publish();
+		case 'force_publish':
+			return plugin_draft_force_publish();
 		default:
 			return plugin_draft_list();
 	}
@@ -154,8 +156,8 @@ function plugin_draft_publish()
 {
 	global $vars, $script;
 	global $_msg_draft_published, $_msg_draft_publish_error, $_msg_draft_publish, $_msg_draft_list;
-	global $_msg_draft_not_found, $_msg_draft_invalid_action, $_msg_draft_conflict_title, $_msg_draft_conflict_body;
-	global $_msg_draft_edit;
+	global $_msg_draft_not_found, $_msg_draft_invalid_action, $_msg_draft_conflict_title, $_msg_draft_conflict_body, $_msg_draft_conflict_diff;
+	global $_msg_draft_edit, $_btn_draft_force_publish, $_msg_draft_force_publish_confirm;
 
 	if ($_SERVER['REQUEST_METHOD'] !== 'POST' || !check_ticket()) {
 		return array(
@@ -186,9 +188,7 @@ function plugin_draft_publish()
 
 	// Conflict detection (digest優先、無ければtimestamp)
 	$current_source = get_source($page, TRUE, TRUE);
-	if ($current_source === FALSE) {
-		$current_source = '';
-	}
+	if ($current_source === FALSE) $current_source = '';
 	$current_digest = md5($current_source);
 
 	$draft_digest = $draft['meta']['digest'];
@@ -210,12 +210,77 @@ function plugin_draft_publish()
 	}
 
 	if ($conflict) {
+		require_once(LIB_DIR . 'diff.php');
+		$diff_raw = do_diff($current_source, $draft['content']);
+		$diff_html = diff_style_to_css(htmlsc($diff_raw));
+		$ticket = get_ticket();
 		$edit_link = $script . '?cmd=edit&amp;page=' . rawurlencode($page) . '&amp;load_draft=true';
 		$body  = '<p>' . $_msg_draft_conflict_body . '</p>';
 		$body .= '<p><a href="' . $edit_link . '">' . $_msg_draft_edit . '</a></p>';
+		$body .= '<h3>' . $_msg_draft_conflict_diff . '</h3>';
+		$body .= '<pre class="diff">' . $diff_html . '</pre>';
+		$body .= '<form action="' . $script . '" method="post" style="margin-top:10px;">' .
+			'<input type="hidden" name="cmd" value="draft" />' .
+			'<input type="hidden" name="action" value="force_publish" />' .
+			'<input type="hidden" name="page" value="' . htmlsc($page) . '" />' .
+			'<input type="hidden" name="ticket" value="' . $ticket . '" />' .
+			'<input type="submit" value="' . $_btn_draft_force_publish . '" onclick="return confirm(\'' . $_msg_draft_force_publish_confirm . '\')" />' .
+			'</form>';
 		return array(
 			'msg' => $_msg_draft_conflict_title,
 			'body' => $body
+		);
+	}
+
+	// Write to main page
+	page_write($page, $draft['content']);
+
+	// Delete draft
+	draft_delete($page);
+
+	$page_link = make_pagelink($page);
+	$body = '<p>' . $_msg_draft_published . ': ' . $page_link . '</p>';
+	$body .= '<p><a href="' . $script . '?cmd=draft">' . $_msg_draft_list . 'に戻る</a></p>';
+
+	return array(
+		'msg' => $_msg_draft_publish,
+		'body' => $body
+	);
+}
+
+/**
+ * Force publish draft (skip conflict check)
+ */
+function plugin_draft_force_publish()
+{
+	global $vars, $script;
+	global $_msg_draft_published, $_msg_draft_publish_error, $_msg_draft_publish, $_msg_draft_list;
+	global $_msg_draft_not_found, $_msg_draft_invalid_action;
+
+	if ($_SERVER['REQUEST_METHOD'] !== 'POST' || !check_ticket()) {
+		return array(
+			'msg' => 'Error',
+			'body' => '<p>' . $_msg_draft_invalid_action . '</p>'
+		);
+	}
+
+	$page = isset($vars['page']) ? $vars['page'] : '';
+	if ($page === '') {
+		return array(
+			'msg' => 'エラー',
+			'body' => '<p>ページが指定されていません。</p>'
+		);
+	}
+
+	// Check permission
+	check_editable($page, true, true);
+
+	// Get draft content
+	$draft = get_draft_with_meta($page, TRUE, TRUE);
+	if ($draft === FALSE || $draft['content'] === '') {
+		return array(
+			'msg' => 'エラー',
+			'body' => '<p>' . $_msg_draft_not_found . '</p>'
 		);
 	}
 
